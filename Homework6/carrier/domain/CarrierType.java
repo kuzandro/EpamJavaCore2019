e3e2aed849b954a48d9d586e975b47ce45017dc8
kuzandro <kuzandro@gmail.com>
2019-12-16 14:57:03 +0300

    Homework6 init

---
+++ Homework6/carrier/domain/CarrierType.java
@@ -0,0 +1,8 @@
+package ru.epam.javacore.lesson_6.homework.carrier.domain;
+
+public enum CarrierType {
+    SHIP,
+    PLANE,
+    CAR,
+    TRAIN
+}
---
+++ Homework6/common/domain/BaseEntity.java
@@ -0,0 +1,13 @@
+package ru.epam.javacore.lesson_6.homework.common.domain;
+
+public abstract class BaseEntity {
+  protected Long id;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+}
